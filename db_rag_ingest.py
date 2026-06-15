4e62ade7e07713cd25c3281ff7e294f360d89a39
Manwel Wasfy <manwelwasfystu@gmail.com>
2026-06-15 08:54:17 +0000

    ..

--- db_rag_ingest.py
+++ db_rag_ingest.py
@@ -1,5 +1,11 @@
 from sqlitesearch import TextSearchIndex
-
 from ingest import load_faq_data
 documents = load_faq_data()
 
+index = TextSearchIndex(
+    text_fields= ['question','section','answer'],
+    keyword_fields=['course'],
+    db_path="FAQ.db"
+)
+
+index.fit(documents)
